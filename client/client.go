93113795270b733ef05517a06dbad5bcb0356ae0
Krzysztof Sikora <prezes.sikora@gmail.com>
2025-03-18 10:17:48 +0100

    Log event ID when fetching event in GetEvent method 6th time.

--- client/client.go
+++ client/client.go
@@ -38,6 +38,7 @@ func (service Events) GetEvent(eventId int) (*Event, error) {
 	log.Printf("Fetching event with id3: [%v]", eventId)
 	log.Printf("Fetching event with id4: [%v]", eventId)
 	log.Printf("Fetching event with id4: [%v]", eventId)
+	log.Printf("Fetching event with id5: [%v]", eventId)
 	c := http.Client{Timeout: service.timeout}
 	url := fmt.Sprintf(service.url, eventId)
 
